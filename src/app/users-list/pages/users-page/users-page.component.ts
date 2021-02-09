44f35d03245bfb53991b9537ad24d13491ba4f92
ArtyomMakarov <Makar6031994@gmail.com>
2021-02-09 18:50:48 +0300

    make the page support browser refresh

--- src/app/users-list/pages/users-page/users-page.component.ts
+++ src/app/users-list/pages/users-page/users-page.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { IUserModel } from "../../models";
 import { UsersHTTPService } from "../../services";
+import { Router } from '@angular/router';
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: 'app-users-page',
@@ -13,18 +15,33 @@ export class UsersPageComponent implements OnInit {
   public isModalOpened: boolean;
   public userInfo: IUserModel;
 
-  constructor(private usersHttpService: UsersHTTPService) { }
+  constructor(private usersHttpService: UsersHTTPService,
+              private route: Router, private activateRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.usersHttpService.loadUsersData().subscribe( res => this.users = res );
+
+    this.activateRoute.queryParams.subscribe( params => {
+
+      this.usersHttpService.loadUsersData().subscribe( res => {
+
+        this.users = res;
+        let user = this.users.filter( user => user.id == params['user']);
+        this.userInfo = user[0];
+        this.isModalOpened = params['modal'];
+
+      } );
+    });
   }
 
   openModalWindow(user: IUserModel): void {
+    this.route.navigate(['/users'], { queryParams: { modal: 'true', user: `${user.id}`}});
     this.isModalOpened = true;
     this.userInfo = user;
   }
 
   closeModalWindow(val: boolean): void {
+    this.route.navigate(['/users']);
     this.isModalOpened = val;
   }
 
